7423494f0e19d551ec234a8d48d9ec2df903c902
Biasn <tatakaeprimal8053@gmail.com>
2025-05-27 21:45:58 -0300

    Update classificacao.py

--- package/classificacao.py
+++ package/classificacao.py
@@ -1,1 +1,10 @@
-
+def classificar_imc(imc: float) -> str:
+    imc = self.calculo_do_imc()
+    if imc < 18.5:
+      return "Magreza"
+    elif 18.5 <= imc < 24.9:
+      return "Normal"
+    elif 25 <= imc < 29.9:
+      return "Sobrepeso"
+    else:
+      return "Obesidade"
