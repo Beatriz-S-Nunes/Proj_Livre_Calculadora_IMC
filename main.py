fc19ce0b4864f258a68448eef587a2dcc283ebfe
Biasn <tatakaeprimal8053@gmail.com>
2025-05-27 21:33:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from package.pessoa import Pessoa
+from package.controlador import ControladorIMC
 
+def main():
+  controlador = ControladorIMC
+
+  while True:
+    nome = input("Nome da pessoa (ou 'sair'): ")
+    if nome.lower() == 'sair':
+      break
+    peso == float(input("Peso (kg): "))
+    altura == float(input("Altura (m): "))
+   
+    pessoa = Pessoa(nome, peso, altura)
+    controlador.adicionar_pessoa(pessoa)
+     
+  print("\nResultados:")
+  controlador.mostrar_todos()
