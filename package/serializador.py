ca4c0ca58cbf9e526db7f20d6ff6b6ba5792a926
Biasn <tatakaeprimal8053@gmail.com>
2025-05-30 23:35:03 -0300

    Update serializador.py

--- package/serializador.py
+++ package/serializador.py
@@ -1,13 +1,31 @@
 import json
+from typing import List
+from package.pessoa import Pessoa 
 
-def salvar_pessoas(pessoas: list,caminho: str = "pessoas.json"):
-  dados = [str(p) for p in pessoas]
-  with open(caminho, "w", encoding="utf-8") as f:
-    json.dump(dados, f, indent=2)
-
-def carregar_pessoas(caminho: str = "pessoas.json") -> list:
+def salvar_pessoas(pessoas: List[Pessoa],caminho: str = "pessoas.json") -> None:
+  dados = []
+  for pessoa in pessoas:
+        dados.append({
+            "nome": pessoa._Pessoa__nome,  ributo privado __nome
+            "peso": pessoa._Pessoa__peso,   
+            "altura": pessoa._Pessoa__altura
+        })
   try:
-    with open(caminho, "r", encoding="utf-8") as f:
-      return json.load(f)
-  except FileNotFoundError:
-    return []
+    with open(caminho, "w", encoding="utf-8") as arquivo:
+      json.dump(dados, arquivo, indent=2, ensure_ascii=False)
+  except (IOError, PermissionError) as e:
+    print(f"Ocorreu um erro ao salvar arquivo: {e}")
+
+def carregar_pessoas(caminho: str = "pessoas.json") -> List[Pessoa]:
+  try:
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+      dados = json.load(arquivo)
+            return [Pessoa(p["nome"], p["peso"], p["altura"]) for p in dados]
+    except FileNotFoundError:
+        return []  # Retorna lista vazia se o arquivo não existir
+    except (json.JSONDecodeError, KeyError) as e:
+        print(f"Erro ao ler arquivo: {e}")
+        return []
+    except (IOError, PermissionError) as e:
+        print(f"Erro ao acessar arquivo: {e}")
+        return []
