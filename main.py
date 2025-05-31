8220a7ce79aff0ab1fa9a848a919934c5c82bb5b
Biasn <tatakaeprimal8053@gmail.com>
2025-05-30 21:24:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     altura = float(input("Altura (m): "))
    
     pessoa = Pessoa(nome, peso, altura)
-    controlador.adicionar_pessoa(self, pessoa)
+    controlador.adicionar_pessoa(pessoa)
      
   print("\nResultados:")
   controlador.mostrar_todos()
