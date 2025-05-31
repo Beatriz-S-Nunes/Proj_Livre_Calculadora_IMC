4e7cb6116f23bb94e2d5f8a2347f550292b74a5e
Biasn <tatakaeprimal8053@gmail.com>
2025-05-30 21:20:12 -0300

    Update controlador.py

--- package/controlador.py
+++ package/controlador.py
@@ -4,7 +4,7 @@ class ControladorIMC:
   def __init__(self):
     self.__lista_pessoas = []
 
-  def adicionar_pessoa(pessoa: Pessoa):
+  def adicionar_pessoa(self, pessoa: Pessoa):
     self.__lista_pessoas.append(pessoa)
 
   def mostrar_todos(self):
