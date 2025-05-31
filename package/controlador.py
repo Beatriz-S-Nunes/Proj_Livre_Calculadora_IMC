1659e729b45ba8b53acc8b2393ed3d4a80456330
Biasn <tatakaeprimal8053@gmail.com>
2025-05-30 23:17:02 -0300

    Update controlador.py

--- package/controlador.py
+++ package/controlador.py
@@ -1,8 +1,14 @@
 from package.pessoa import Pessoa
+from package.serializador import salvar_pessoas
 
 class ControladorIMC:
   def __init__(self):
     self.__lista_pessoas = []
+    self.carregar_dados()
+
+  def carregar_dados(self):
+    from package.serializador import carregar_pessoas
+    self.__lista_pessoas = carregar_pessoas()
 
   def adicionar_pessoa(self, pessoa: Pessoa):
     self.__lista_pessoas.append(pessoa)
