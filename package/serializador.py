05cdac82d35a2a756dff318102318963dfa4a6e6
Biasn <tatakaeprimal8053@gmail.com>
2025-06-05 15:37:26 -0300

    Update serializador.py

--- package/serializador.py
+++ package/serializador.py
@@ -7,16 +7,16 @@ def salvar_pessoas(pessoas: List[Pessoa],caminho: str = "pessoas.json") -> None:
              for p in pessoas]
   try:
     with open(caminho, "w", encoding="utf-8") as f:
-            json.dump(dados, f, indent=2, ensure_ascii=False)
+      json.dump(dados, f, indent=2, ensure_ascii=False)
   except Exception as e:
     raise IOError(f"Falha ao salvar: {str(e)}")
 
-def carregar_pessoas(caminho: str = "pessoas.json") -> List[Pessoas]:
+def carregar_pessoas(caminho: str = "pessoas.json") -> List[Pessoa]':
   try:
     with open(caminho, "r", encoding="utf-8") as f:
-            dados = json.load(f)
-            return [Pessoa(p["nome"], p["peso"], p["altura"]) for p in dados]
+      dados = json.load(f)
+      return [Pessoa(p["nome"], p["peso"], p["altura"]) for p in dados]
   except FileNotFoundError:
-      return []
+    return []
   except Exception as e:
     raise IOError(f"Falha ao carregar: {str(e)}")
